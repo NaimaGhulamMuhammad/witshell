46fb2759238940b562c5c3948e499c91d2a70a84
Naima Ghulam Muhammad <63257312+NaimaGhulamMuhammad@users.noreply.github.com>
2022-07-08 18:30:49 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      
+      <h2>WITSHELL....</h2><br/>
+      <marquee>Coming Soon</marquee>
+    </main>
+  );
+}
+
+export default App;
